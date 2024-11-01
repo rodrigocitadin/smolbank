83cd5af7aad5e57a9ad3b32c92eda7b46f21801e
rodrigocitadin <citadinrodrigo@gmail.com>
2024-11-01 17:21:29 -0300

    add password field to client

--- priv/repo/migrations/20241031133049_create_clients.exs
+++ priv/repo/migrations/20241031133049_create_clients.exs
@@ -5,6 +5,7 @@ defmodule CumbucaChallenge.Repo.Migrations.CreateClients do
     create table(:clients, primary_key: false) do
       add :id, :uuid, primary_key: true
       add :name, :string, null: false
+      add :password, :string, null: false
       add :cpf, :string, null: false
       add :balance, :integer, null: false
 
