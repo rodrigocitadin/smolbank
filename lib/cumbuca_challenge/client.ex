3fec5a42a1df61030a9b20e7a6f8487a35836906
rodrigocitadin <citadinrodrigo@gmail.com>
2024-11-01 18:11:53 -0300

    hashing password before insert

--- lib/cumbuca_challenge/client.ex
+++ lib/cumbuca_challenge/client.ex
@@ -1,22 +1,36 @@
 defmodule CumbucaChallenge.Client do
   use CumbucaChallenge.Schema
 
-  @client_params [:name, :cpf]
+  @client_params [:name, :cpf, :password]
 
   schema "clients" do
     field :name, :string
     field :cpf, :string
+    field :password, :string
     field :balance, :integer, default: 0
 
     timestamps()
   end
 
-  def changeset(client, params \\ %{}) do
+  def changeset(params), do: create_changeset(%__MODULE__{}, params)
+  def changeset(client, params), do: create_changeset(client, params)
+
+  def create_changeset(client, params) do
     client
     |> Ecto.Changeset.cast(params, @client_params)
     |> Ecto.Changeset.validate_required(@client_params)
     |> Ecto.Changeset.validate_format(:cpf, ~r/^[[:digit:]]+$/)
     |> Ecto.Changeset.validate_length(:cpf, is: 11)
+    |> Ecto.Changeset.validate_length(:password, min: 6)
     |> Ecto.Changeset.unique_constraint(:cpf)
+    |> put_password_hash()
   end
+
+  defp put_password_hash(
+         %Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset
+       ) do
+    Ecto.Changeset.change(changeset, password: Argon2.hash_pwd_salt(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
