a319594706819ae87392f3a5c59ed82ae0fdf546
rodrigocitadin <citadinrodrigo@gmail.com>
2025-01-12 01:39:34 -0300

    wip in revoke

--- lib/smolbank/transactions.ex
+++ lib/smolbank/transactions.ex
@@ -1,5 +1,7 @@
 defmodule Smolbank.Transactions do
+  alias Smolbank.Transactions.Get
   alias Smolbank.Transactions.Create
 
   defdelegate create(params), to: Create, as: :call
+  defdelegate get(id), to: Get, as: :call
 end
