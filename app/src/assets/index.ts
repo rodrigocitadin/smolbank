e83cbaafa03223803c62cd6e2d9f228d1c8f9b7b
rodrigocitadin <citadinrodrigo@gmail.com>
2025-01-14 15:15:36 -0300

    assets icons

---
+++ app/src/assets/index.ts
@@ -0,0 +1,9 @@
+import SendTransactionIcon from "@/assets/money-send-circle-stroke-rounded.svg"
+import PayDebtsIcon from "@/assets/money-receive-circle-stroke-rounded.svg"
+import RefundTransactionIcon from "@/assets/transaction-stroke-rounded.svg"
+
+export {
+  SendTransactionIcon,
+  PayDebtsIcon,
+  RefundTransactionIcon
+}
