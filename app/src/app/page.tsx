bac8a15943573188c1e804b149c9e820ed190feb
rodrigocitadin <citadinrodrigo@gmail.com>
2025-01-14 15:15:56 -0300

    wip in home page

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,101 +1,60 @@
+import { PayDebtsIcon, SendTransactionIcon } from "@/assets";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+type Action = {
+  id: number
+  icon: any
+  text: string
+}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+export default function Home() {
+  const actions: Action[] = [
+    {
+      id: 1,
+      icon: SendTransactionIcon,
+      text: "New transaction"
+    },
+    {
+      id: 2,
+      icon: PayDebtsIcon,
+      text: "Pay debts"
+    },
+  ]
+
+  return (
+    <>
+      <div className="mx-4 my-8">
+        <h1 className="text-4xl">Hi, Rodrigo!</h1>
+        <h2 className="text-2xl">How are your finances today?</h2>
+      </div>
+      <main className="mx-4">
+        <section>
+          <MoneyCard text="Balance:" amount={100} />
+          <MoneyCard text="Debt:" amount={50} negative />
+        </section>
+        <section className="flex flex-col gap-2 my-8">
+          {actions.map(action => <ActionButton key={action.id} action={action} />)}
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
+}
+
+function MoneyCard({ text, amount, negative }: { text: string, amount: number, negative?: boolean }) {
+  return (
+    <div className="flex justify-between text-xl">
+      <p>{text}</p>
+      <strong className={negative ? "text-red-500" : ""}>{amount.toFixed(2)} $</strong>
+    </div>
+  )
+}
+
+function ActionButton({ action }: { action: Action }) {
+  return (
+    <div className="flex items-center gap-2">
+      <Image src={action.icon} alt={action.text} />
+      <span>{action.text}</span>
+      {/* {action.text.split(" ").map(text => <span key={text}>{text}</span>)} */}
+    </div>
+  )
 }
