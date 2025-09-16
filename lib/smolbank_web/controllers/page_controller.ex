c198eb3912cf0e6d82104560f7a2d23ede3cf3c5
rodrigocitadin <citadinrodrigo@gmail.com>
2025-09-16 18:56:39 -0300

    chore: css and removal of useless things

--- lib/smolbank_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule SmolbankWeb.PageController do
-  use SmolbankWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
